54a5c960b1b8635f5b9ad3fcadf4534e373b3e8d
Anil Muraleedharan <56071561+anil-muraleedharan@users.noreply.github.com>
2020-02-11 16:09:17 +0530

    [ Anil | Saurabh ] Added tests for GET method

---
+++ test/testHandlers.js
@@ -0,0 +1,34 @@
+const request = require('supertest');
+const { app } = require('../lib/handler');
+
+describe('GET', () => {
+  it('should give homepage on "/" req', done => {
+    request(app.serve.bind(app))
+      .get('/')
+      .expect(200, done)
+      .expect('Content-Type', 'text/html');
+  });
+
+  it('should give css for /style.css', done => {
+    request(app.serve.bind(app))
+      .get('/style.css')
+      .expect(200, done)
+      .expect('Content-Type', 'text/css');
+  });
+
+  it('should fetch all todo content on /getTodoLists', done => {
+    request(app.serve.bind(app))
+      .get('/getTodoLists')
+      .expect(200, done)
+      .expect('Content-Type', 'application/json');
+  });
+
+  it('should return an error: page not found on /badUrl', done => {
+    request(app.serve.bind(app))
+      .get('/badUrl')
+      .expect(404, done)
+      .expect(/pageNotFound/)
+      .expect('Content-Type', 'text/plain');
+  });
+});
+0;
