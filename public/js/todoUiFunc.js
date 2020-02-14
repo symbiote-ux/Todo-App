0c2d035e8662044ee52eda1c23d82c3bdb737749
Saurabh <58027263+symbiote-ux@users.noreply.github.com>
2020-02-14 14:24:21 +0530

    [ Anil | Saurabh ] Pulled out Front end and script Xml request things to different files

---
+++ public/js/todoUiFunc.js
@@ -0,0 +1,81 @@
+let currentIdNo = 0;
+let taskIdList = [];
+
+const makeDisplayBlock = id => {
+  document.getElementById(id).style.display = 'block';
+};
+
+const makeDisplayNone = id => {
+  document.getElementById(id).style.display = 'none';
+};
+
+const openNewTodo = () => {
+  ['todo-title', 'close-todo', 'save-todo'].forEach(makeDisplayBlock);
+  makeDisplayNone('add-todo');
+};
+
+const closeNewTodo = () => {
+  document.getElementById('title').value = '';
+  ['todo-title', 'close-todo', 'save-todo'].forEach(makeDisplayNone);
+  makeDisplayBlock('add-todo');
+  for (let idNo = 0; idNo < taskIdList.length; idNo++) {
+    const element = document.getElementById(`todo-element-${taskIdList[idNo]}`);
+    element.parentNode.removeChild(element);
+  }
+  currentIdNo = 0;
+  taskIdList = [];
+};
+
+const insertTask = id => {
+  const [, , idNo] = id.split('-');
+  document.getElementById(id).style.display = 'none';
+  document.getElementById(`insert-task-${idNo}`).className = 'insert-task-box';
+};
+
+const removeTask = id => {
+  const [, idNoToRemove] = id.split('-');
+  const index = taskIdList.indexOf(+idNoToRemove);
+  if (index > -1) {
+    taskIdList.splice(index, 1);
+  }
+  const element = document.getElementById(`todo-element-${idNoToRemove}`);
+  element.parentNode.removeChild(element);
+};
+
+const isTitleEmpty = () => {
+  taskIdList.length === 0 && document.querySelector('#title').value === '';
+};
+
+const isElementEmpty = () =>
+  taskIdList.length !== 0 &&
+  document.querySelector(`#todo-element-${currentIdNo} input[name="element"]`).value === '';
+
+const addTask = () => {
+  if (isTitleEmpty()) {
+    return;
+  }
+  if (isElementEmpty()) {
+    return;
+  }
+  currentIdNo++;
+  const htmlToAdd = createTaskHtml(currentIdNo);
+  taskIdList.push(currentIdNo);
+  document.querySelector('#todo-tasks').appendChild(htmlToAdd);
+};
+
+const clickSaveInsertedButton = idNo => {
+  if (event.keyCode === 13) {
+    document.querySelector(`#save-inserted-${idNo}`).click();
+  }
+};
+
+const clickAddTaskButton = id => {
+  if (event.keyCode === 13) {
+    document.querySelector('#add-task').click();
+    const elementToFocus =
+      id === 'title'
+        ? document.querySelector('#task-input-1')
+        : document.querySelector(`#task-input-${id + 1}`);
+    elementToFocus.focus();
+  }
+};
--- public/js/xmlReq.js
+++ public/js/xmlReq.js
@@ -1,79 +1,3 @@
-let currentIdNo = 0;
-let taskIdList = [];
-
-const makeDisplayBlock = id => {
-  document.getElementById(id).style.display = 'block';
-};
-
-const makeDisplayNone = id => {
-  document.getElementById(id).style.display = 'none';
-};
-
-const openNewTodo = () => {
-  ['todo-title', 'close-todo', 'save-todo'].forEach(makeDisplayBlock);
-  makeDisplayNone('add-todo');
-};
-
-const closeNewTodo = () => {
-  document.getElementById('title').value = '';
-  ['todo-title', 'close-todo', 'save-todo'].forEach(makeDisplayNone);
-  makeDisplayBlock('add-todo');
-  for (let idNo = 0; idNo < taskIdList.length; idNo++) {
-    const element = document.getElementById(`todo-element-${taskIdList[idNo]}`);
-    element.parentNode.removeChild(element);
-  }
-  currentIdNo = 0;
-  taskIdList = [];
-};
-
-const removeTask = id => {
-  const [, idNoToRemove] = id.split('-');
-  const index = taskIdList.indexOf(+idNoToRemove);
-  if (index > -1) {
-    taskIdList.splice(index, 1);
-  }
-  const element = document.getElementById(`todo-element-${idNoToRemove}`);
-  element.parentNode.removeChild(element);
-};
-
-const isTitleEmpty = () => {
-  taskIdList.length === 0 && document.querySelector('#title').value === '';
-};
-
-const isElementEmpty = () =>
-  taskIdList.length !== 0 &&
-  document.querySelector(`#todo-element-${currentIdNo} input[name="element"]`).value === '';
-
-const addTask = () => {
-  if (isTitleEmpty()) {
-    return;
-  }
-  if (isElementEmpty()) {
-    return;
-  }
-  currentIdNo++;
-  const htmlToAdd = createTaskHtml(currentIdNo);
-  taskIdList.push(currentIdNo);
-  document.querySelector('#todo-tasks').appendChild(htmlToAdd);
-};
-
-const clickSaveInsertedButton = idNo => {
-  if (event.keyCode === 13) {
-    document.querySelector(`#save-inserted-${idNo}`).click();
-  }
-};
-
-const clickAddTaskButton = id => {
-  if (event.keyCode === 13) {
-    document.querySelector('#add-task').click();
-    const elementToFocus =
-      id === 'title'
-        ? document.querySelector('#task-input-1')
-        : document.querySelector(`#task-input-${id + 1}`);
-    elementToFocus.focus();
-  }
-};
-
 const formatContents = function() {
   const todoLists = JSON.parse(this.responseText);
   const formattedTodo = todoLists.map(generateHtmlForSavedTodo);
@@ -82,12 +6,6 @@ const formatContents = function() {
   formattedTodo.forEach(element => {
     parentElement.appendChild(element);
   });
-};
-
-const insertTask = id => {
-  const [, , idNo] = id.split('-');
-  document.getElementById(id).style.display = 'none';
-  document.getElementById(`insert-task-${idNo}`).className = 'insert-task-box';
 };
 
 const editTask = taskId => {
@@ -193,11 +111,11 @@ const fetchAllTodo = callback => {
 };
 
 const sendXHR = (data, url, method, callBack) => {
-  const request = new XMLHttpRequest();
-  request.open(method, url);
-  request.setRequestHeader('Content-Type', 'application/json');
-  request.send(data);
-  request.onload = callBack;
+  const req = new XMLHttpRequest();
+  req.open(method, url);
+  req.setRequestHeader('Content-Type', 'application/json');
+  req.send(data);
+  req.onload = callBack;
 };
 
 window.onload = fetchAllTodo(formatContents);
