4272490923f0de77aa9e7f4fb3a234aa41b28070
Anil Muraleedharan <56071561+anil-muraleedharan@users.noreply.github.com>
2020-02-14 16:03:35 +0530

    [ Anil | Saurabh ] Added test for todo class findTask method

--- test/testTodo.js
+++ test/testTodo.js
@@ -32,4 +32,21 @@ describe('TODO class', () => {
       deepStrictEqual(todo.generateId(), '1-1');
     });
   });
+
+  describe('findTask', function() {
+    const todo = Todo.createNewTodo({
+      id: 1,
+      title: 'abc',
+      tasks: [{ id: '1-1', status: false, content: 'abc' }]
+    });
+
+    it('should find a task matching to the given id', () => {
+      const task = { id: '1-1', status: false, content: 'abc' };
+      deepStrictEqual(todo.findTask('1-1'), task);
+    });
+
+    it('should give undefined if the id does not match with any task', () => {
+      deepStrictEqual(todo.findTask('1-2'));
+    });
+  });
 });
