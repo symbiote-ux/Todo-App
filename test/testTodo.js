ae1b0effe07445369bc877799b3cc8e94047bc4b
Anil Muraleedharan <56071561+anil-muraleedharan@users.noreply.github.com>
2020-02-14 16:02:50 +0530

    [ Anil | Saurabh ] Added test for todo class generateId method

--- test/testTodo.js
+++ test/testTodo.js
@@ -12,4 +12,24 @@ describe('TODO class', () => {
       instanceOf(todo, Todo);
     });
   });
+
+  describe('generateId', () => {
+    it('should generate id for new task depending on the last task id', () => {
+      const todo = Todo.createNewTodo({
+        id: 1,
+        title: 'abc',
+        tasks: [{ id: '1-1', status: false, content: 'abc' }]
+      });
+      deepStrictEqual(todo.generateId(), '1-2');
+    });
+
+    it('should generate id if the task list is empty', () => {
+      const todo = Todo.createNewTodo({
+        id: 1,
+        title: 'abc',
+        tasks: []
+      });
+      deepStrictEqual(todo.generateId(), '1-1');
+    });
+  });
 });
