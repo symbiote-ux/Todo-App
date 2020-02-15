e6a338a33366e053072b84ca3d142fd386e72c31
Anil Muraleedharan <56071561+anil-muraleedharan@users.noreply.github.com>
2020-02-15 09:46:09 +0530

    [ Anil | Saurabh ] Added test for TodoList class findTodo method

--- test/testTodoList.js
+++ test/testTodoList.js
@@ -63,4 +63,42 @@ describe('TodoList Class', () => {
       deepStrictEqual(todoList.generateId(), 1);
     });
   });
+
+  describe('findTodo', () => {
+    it('should find todo matching to the given id', () => {
+      const todoList = TodoList.load([
+        {
+          id: 1,
+          title: 'abc',
+          tasks: [{ id: '1-1', status: false, content: 'def' }],
+          timeStamp
+        },
+        {
+          id: 2,
+          title: 'pqr',
+          tasks: [{ id: '2-1', status: false, content: 'xyz' }],
+          timeStamp
+        }
+      ]);
+      const expectedTodo = Todo.createNewTodo({
+        id: 2,
+        title: 'pqr',
+        tasks: [{ id: '2-1', status: false, content: 'xyz' }],
+        timeStamp
+      });
+      deepStrictEqual(todoList.findTodo(2), expectedTodo);
+    });
+
+    it('should give undefined if the id does not exist', () => {
+      const todoList = TodoList.load([
+        {
+          id: 1,
+          title: 'abc',
+          tasks: [{ id: '1-1', status: false, content: 'def' }],
+          timeStamp
+        }
+      ]);
+      deepStrictEqual(todoList.findTodo(3));
+    });
+  });
 });
