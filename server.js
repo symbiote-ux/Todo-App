8c2592a166ac836d22c5ca63862027e0d7dbef2b
Anil Muraleedharan <56071561+anil-muraleedharan@users.noreply.github.com>
2020-03-21 15:49:48 +0530

    Added morgan for logging the requests.

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const { app } = require('./lib/routes');
 const main = () => {
   const defaultPort = 4000;
   const port = process.env.PORT || defaultPort;
-  app.listen(port, () => process.stdout.write(`started listening at ${port}`));
+  app.listen(port, () =>
+    process.stdout.write(`started listening at ${port}\n\n`)
+  );
 };
 
 main();
